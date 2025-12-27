a9373fe8cb65f2650db5ddb4d3828c73b4687c9c
TamoorArif <tamoorarif44445@gmail.com>
2025-12-27 20:41:58 +0500

    New Arrival Spec is WIP

--- playwright/utils/selectors/home/index.js
+++ playwright/utils/selectors/home/index.js
@@ -3,3 +3,16 @@ export const HOME_SELECTORS = {
     AGE_MODAL: '#mc_modal',
     AGE_CONFIRM_BUTTON: '.age-actions .btn.btn-over',
 };
+
+// New arival selectors
+export const NEW_ARRIVALS_SELECTORS = {
+    NEW_ARRIVALS_LINK: '#auto_id_20',
+    PRODUCTS_GRID: '#products_grid .col-lg-3',
+    PRODUCT_LINKS: 'a[href*="/shop/"]:has(img)',
+    PRODUCT_NAME_SELECTORS: [
+        'h1.product-name',
+        'h1',
+        '.product-name',
+        '[class*="product"] h1'
+    ],
+};
